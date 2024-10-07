ee3a1fc59adddffc1cb8f3afacd4d717dc86c519
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2024-10-07 13:02:02 -0300

    Criação das funções para iniciar uma lista de estudantes aleatória

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
-	listaDeEstudantes := make([]*estudante, 0)
+	estudantes := iniciarListaDeEstudantes()
+    estudantes = registrarIncompatibilidades(estudantes)
+    fmt.Printf("estudantes: %v\n", estudantes)
+}
+
+func iniciarListaDeEstudantes() []estudante {
+	listaDeEstudantes := make([]estudante, 0)
 	for i := 0; i < 100; i++ {
 		var curso Curso
 		if i%2 == 0 {
@@ -11,9 +20,34 @@ func main() {
 		} else {
 			curso = C2
 		}
-		listaDeEstudantes = append(listaDeEstudantes, &estudante{nome: "Estudante" + fmt.Sprint(i), curso: curso})
-        fmt.Print(*listaDeEstudantes[i])
+		listaDeEstudantes = append(listaDeEstudantes, estudante{nome: "Estudante" + fmt.Sprint(i), curso: curso})
 	}
+	return listaDeEstudantes
+}
+
+func registrarIncompatibilidades(estudantes []estudante) []estudante {
+	randomSource := rand.NewSource(123312) // número pré-definido para facilitar os testes
+	random := rand.New(randomSource)
+	for i := 0; i < len(estudantes); i++ {
+		quantidadeDeIncompatíveis := random.Intn(5)
+		for j := 0; j < quantidadeDeIncompatíveis; j++ {
+			var estudanteIncompatívelAleatório int
+
+			// tem que ser um loop porque não é permitido um aluno ser incompatível com ele mesmo
+			for true {
+				estudanteIncompatívelAleatório = random.Intn(len(estudantes) - 1)
+				if estudanteIncompatívelAleatório != i {
+					break
+				}
+			}
+
+			estudantes[i].estudantesIncompatíveis = append(
+				estudantes[i].estudantesIncompatíveis,
+				&estudantes[estudanteIncompatívelAleatório],
+			)
+		}
+	}
+	return estudantes
 }
 
 type Curso int
@@ -24,6 +58,7 @@ const (
 )
 
 type estudante struct {
-	nome  string
-	curso Curso
+	nome                    string
+	curso                   Curso
+	estudantesIncompatíveis []*estudante
 }
